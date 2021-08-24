c08177b874959eeb0934ceb7a1738171e37b517e
kamta0905 <88268903+kamta0905@users.noreply.github.com>
2021-08-24 19:50:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
   return (
     
     <div id="main">
-      // Do not alter the main div
-     <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>
-      <button id="click" onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+       {show ? null : <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+            <button id="click" onClick={function name() { setShow(false) }} >
+                Click me
+            </button>
     </div>
   );
 }
