e1292c27125f1d735025138266aac29e6e219ded
kamta0905 <88268903+kamta0905@users.noreply.github.com>
2021-08-24 17:26:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div id="main">
       // Do not alter the main div
+     <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
